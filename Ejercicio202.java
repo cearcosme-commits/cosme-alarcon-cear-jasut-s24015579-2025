c7ca3e4464a61ce1b52f8a3ed1c017ec25a42a9e
cearcosme-commits <cearcosme@gmail.com>
2025-11-29 18:16:10 -0600

    Add files via upload

---
+++ Ejercicio202.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Ejercicio202{
+public static void main (String args[]){
+	int numero=1;
+	while(numero<= 20 ) {
+	if(numero %2 == 0){
+System.out.println(numero);
+	}
+	numero++;
+		}
+	}
+}
