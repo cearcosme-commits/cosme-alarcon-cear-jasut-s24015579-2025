f4b057df2e7a2f36ca8723304e5c1eaf4ce40c31
cearcosme-commits <cearcosme@gmail.com>
2025-11-29 18:14:33 -0600

    Add files via upload

---
+++ Ejercicio201.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ejercicio201{
+public static void main (String args []){
+	String [] nombres={"ana" , "daniela" , "cear" , "JASUT" , "ariana" , "jaasiel" , "jesus" , "maria" , "ricardo" , "eduardo"};
+	
+	int i = 0;
+	while (i<nombres.length){
+		System.out.println(nombres [i]);
+	i++;
+		}
+	}
+}
+	
